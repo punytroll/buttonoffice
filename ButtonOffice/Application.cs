c6fa94d0578bedbe1b6b23e18bd761526193c9e8
Hagen M?bius <hagen.moebius@googlemail.com>
2011-07-18 09:27:24 +0200

    Because the application has a Windows.Forms GUI the STAThread attribute should be set on the Main function.

--- ButtonOffice/Application.cs
+++ ButtonOffice/Application.cs
@@ -2,6 +2,7 @@
 {
     internal class Application
     {
+        [System.STAThread]
         public static void Main(System.String[] Arguments)
         {
             System.Windows.Forms.Application.Run(new MainWindow());
